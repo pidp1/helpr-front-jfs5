a504408e8c51a5646ffd1ac36d4b0f3c44a8aaa1
polvinh0 <ieiel_4@hotmail.com>
2022-12-08 15:01:02 -0300

    Criação da pagina Editar Funcionarios

---
+++ src/app/views/funcionarios/edit-funcionario/edit-funcionario.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Cargo } from 'src/app/models/cargo';
+import { Funcionario } from 'src/app/models/funcionario';
+import { CargoService } from 'src/app/services/cargo.service';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
+
+@Component({
+  selector: 'app-edit-funcionario',
+  templateUrl: './edit-funcionario.component.html',
+  styleUrls: ['./edit-funcionario.component.css']
+})
+export class EditFuncionarioComponent implements OnInit {
+
+  public funcionario: Funcionario = {
+    nome: '',
+    cpf: '',
+    email: '',
+    cargo: { idCargo: 0, nome: '', descricao: '', salario: 0 },
+    senha: ''
+  }
+
+  public cargos: Cargo[] = []
+
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private cargosService: CargoService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.inizializeCargosField()
+    this.inizializeFields()
+  }
+
+  inizializeCargosField() {
+    this.cargosService.findAll().subscribe(
+      (cargos) => {this.cargos = cargos}
+    )
+  }
+
+  inizializeFields() {
+    const id: string | null = this.route.snapshot.paramMap.get("id")
+    if(id) {
+      this.funcionarioService.findById(id).subscribe(
+        (funcionario) => {this.funcionario = funcionario}
+      )
+    }
+
+  }
+
+  public update(formEdit: NgForm){
+    if(formEdit.valid) {
+      this.funcionarioService.update(this.funcionario).subscribe(
+        (ok)=> {
+          this.toastr.success("Funcionário Editado com sucesso!")
+          this.router.navigateByUrl("/funcionarios")
+        }
+      )
+    } else {
+      this.toastr.error("Dados inválidos!")
+    }
+
+  }
+
+}
--- src/app/views/funcionarios/funcionarios.module.ts
+++ src/app/views/funcionarios/funcionarios.module.ts
@@ -7,12 +7,14 @@ import { ComponentsModule } from 'src/app/components/components.module';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { NewFuncionarioComponent } from './new-funcionario/new-funcionario.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EditFuncionarioComponent } from './edit-funcionario/edit-funcionario.component';
 
 
 @NgModule({
   declarations: [
     FuncionariosComponent,
     NewFuncionarioComponent,
+    EditFuncionarioComponent,
   ],
   imports: [
     CommonModule,
